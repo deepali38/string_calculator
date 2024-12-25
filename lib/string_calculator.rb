2f70d2d33905f0082029677d641423c9812a814c
deepali38 <deepalikumariyadav1234@Gmail.com>
2024-12-25 20:40:57 +0530

    Add support for custom delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,17 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    
-    # Split the input string by commas or newlines into an array of number strings
-    num_array = numbers.split(/,|\n/)
+
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2..].split("\n", 2)
+      delimiter = delimiter.gsub(/[\[\]]/, '')  # Remove brackets if present
+
+      # Split the numbers using the custom delimiter
+      num_array = numbers.split(delimiter)
+    else
+      # Split the input string by commas or newlines into an array of number strings
+      num_array = numbers.split(/,|\n/)
+    end
 
     # Convert the array of number strings to an array of integers
     num_array = num_array.map(&:to_i)
